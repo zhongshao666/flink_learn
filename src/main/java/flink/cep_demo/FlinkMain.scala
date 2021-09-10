0b41f95efd6000990f5c7ba859c6fd137c573412
zhongzhihao <1657602736@qq.com>
2021-09-10 10:56:08 +0800

    feat://cep

--- src/main/java/flink/cep_demo/FlinkMain.scala
+++ src/main/java/flink/cep_demo/FlinkMain.scala
@@ -1,9 +1,8 @@
 package flink.cep_demo
 
 
-
 import org.apache.flink.api.common.eventtime.{SerializableTimestampAssigner, WatermarkStrategy}
-import org.apache.flink.cep.PatternSelectFunction
+import org.apache.flink.cep.{PatternFlatSelectFunction, PatternSelectFunction}
 import org.apache.flink.cep.pattern.conditions.SimpleCondition
 import org.apache.flink.cep.scala.{CEP, PatternStream}
 import org.apache.flink.cep.scala.pattern.Pattern
@@ -14,7 +13,7 @@ import java.sql.Timestamp
 import java.time.format.DateTimeFormatter
 import java.time.{Duration, LocalDateTime, ZoneOffset}
 import java.util
-import scala.collection.convert.ImplicitConversions.`collection AsScalaIterable`
+import scala.collection.convert.ImplicitConversions.{`collection AsScalaIterable`, `map AsScala`}
 
 object FlinkMain {
   def main(args: Array[String]): Unit = {
@@ -36,47 +35,26 @@ object FlinkMain {
     val stream: DataStream[Event] = env
       .fromCollection(events).assignTimestampsAndWatermarks(value)
     //    stream.print("source")
-    val value1: DataStream[Event] = new DataStream[Event](stream.javaStream)// scala->java   java->scala
+    val value1: DataStream[Event] = new DataStream[Event](stream.javaStream) // scala->java   java->scala
 
 
-    val pattern: Pattern[Event, Event] = Pattern.begin[Event]("start").where(_.name.equals("c")) //.optional
-      .followedByAny("second").where(_.name.equals("a")).optional
+    val pattern: Pattern[Event, Event] = Pattern.begin[Event]("start").where((_: Event).name.equals("c")) //.optional
+      .followedByAny("second").where((_: Event).name.equals("a")).optional
 
-    val patternStream: PatternStream[Event] = CEP.pattern(stream,pattern)
-    val resultStream: DataStream[String] = patternStream.select(new PatternSelectFunction[Event,String] {
-      override def select(map: util.Map[String, util.List[Event]]): String = {
-        val keys: util.Iterator[String] = map.keySet().iterator()
-        var e1: String = ""
-        if (map.get("start") != null) {
-          if (!map.get("start").isEmpty) {
-            e1 = map.get("start").toList.head.toString
-          }
-        }
-        var e2: String = ""
-        if (map.get("second") != null) {
-          if (!map.get("second").isEmpty) {
-            e2 = map.get("second").toList.head.toString
-          }
-        }
-        e1 + " " + e2
-      }
+    val patternStream: PatternStream[Event] = CEP.pattern(stream, pattern)
+
+
+    val resultStream: DataStream[String] = patternStream.select(new SelectFunc)
+    val resultStream2: DataStream[String] = patternStream.select((map: collection.Map[String, Iterable[Event]]) => {
+      deal(map)
     })
-    val outputTag = OutputTag[String]("side-output")
-    val resultStream2: DataStream[String] =patternStream.select((map: collection.Map[String, Iterable[Event]]) =>{
-      var e1: String = ""
-      if (map.get("start") != null) {
-        if (map.contains("start")) {
-          e1 = map.get("start").toList.head.toString
-        }
-      }
-      var e2: String = ""
-      if (map.get("second") != null) {
-        if (map.contains("second")) {
-          e2 = map.get("second").toList.head.toString
-        }
-      }
-      e1 + " " + e2
-    })
+
+    val outputTag: OutputTag[String] = OutputTag[String]("side-output")
+    val resultStream3: DataStream[String] = patternStream.flatSelect(new FlatSelectFunc)
+
+    val outStream: DataStream[String] = resultStream3.getSideOutput(outputTag)
+    resultStream3.print("result3")
+    outStream.print("sideOutput")
 
 
     resultStream.print("result")
@@ -84,10 +62,51 @@ object FlinkMain {
     env.execute()
 
 
-    val now: LocalDateTime = LocalDateTime.now
-    val milli: Long = now.toInstant(ZoneOffset.of("+8")).toEpochMilli
-    val l: Long = Timestamp.valueOf(LocalDateTime.now()).getTime
-    val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")
-    val str: String = now.format(formatter)
+    //    val now: LocalDateTime = LocalDateTime.now
+    //    val milli: Long = now.toInstant(ZoneOffset.of("+8")).toEpochMilli
+    //    val l: Long = Timestamp.valueOf(LocalDateTime.now()).getTime
+    //    val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")
+    //    val str: String = now.format(formatter)
+  }
+  def deal(map: collection.Map[String, Iterable[Event]]): String ={
+    val e1: String =
+      if (map.contains("start")) {
+        map.get("start").toList.head.toString
+      } else ""
+    val e2: String = if (map.contains("second")) {
+      map.get("second").toList.head.toString
+    } else ""
+    e1 + " " + e2
   }
 }
+
+class SelectFunc extends PatternSelectFunction[Event, String] {
+  override def select(map: util.Map[String, util.List[Event]]): String = {
+    val e1: String =
+      if (map.contains("start")) {
+        map.get("start").toList.head.toString
+      } else ""
+    val e2: String =
+      if (map.contains("second")) {
+        map.get("second").toList.head.toString
+      } else ""
+    e1 + " " + e2
+  }
+}
+
+class FlatSelectFunc extends PatternFlatSelectFunction[Event,String]{
+  override def flatSelect(map: util.Map[String, util.List[Event]], collector: Collector[String]): Unit = {
+    val e1: String =
+      if (map.contains("start")) {
+        map.get("start").toList.head.toString
+      } else ""
+
+    val e2: String =
+      if (map.contains("second")) {
+        map.get("second").toList.head.toString
+      } else ""
+    collector.collect(e1 + " " + e2)
+  }
+}
+
+//class FlatSelectFunc2 extends
