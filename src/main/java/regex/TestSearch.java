13d9bb71c402abd4060a1bbab985d84a081426c0
zhongshao666 <1657602736@qq.com>
2021-11-24 00:15:42 +0800

    feat://kafka jaas flume

---
+++ src/main/java/regex/TestSearch.java
@@ -0,0 +1,24 @@
+package regex;
+
+import com.fulmicoton.multiregexp.MultiPattern;
+import com.fulmicoton.multiregexp.MultiPatternSearcher;
+
+public class TestSearch {
+    public static void main(String[] args){
+        MultiPatternSearcher multiPatternSearcher = MultiPattern.of(
+                "ab+",     // 0
+                "abc+",    // 1
+                "ab?c",    // 2
+                "v",       // 3
+                "v.*",     // 4
+                "(def)+"   // 5
+        ).searcher();
+        MultiPatternSearcher.Cursor cursor = multiPatternSearcher.search("ab abc vvv");
+        while (cursor.next()) {
+//            int[] pattern = cursor.match();   // array with the pattern id which match ends at this position
+            int pattern = cursor.match();
+            int start = cursor.start();
+            int end = cursor.end();
+        }
+    }
+}
