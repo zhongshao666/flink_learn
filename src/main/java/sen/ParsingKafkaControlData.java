439965d6310e79b58b7976060f378bb98763f406
zhongshao666 <1657602736@qq.com>
2023-02-27 00:02:12 +0800

    feat://kafka jaas flume

--- src/main/java/sen/ParsingKafkaControlData.java
+++
@@ -1,9 +0,0 @@
-package sen;
-
-//import com.alibaba.fastjson.JSON;
-//
-//public class ParsingKafkaControlData {
-//    public static ControlData parsingKafkaControlData(String s) {
-//        return JSON.parseObject(s, ControlData.class);
-//    }
-//}
